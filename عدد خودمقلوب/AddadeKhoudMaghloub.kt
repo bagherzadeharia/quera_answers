9b2305361e073a1e05de3c78044325a22f62400e
Aria Bagherzadeh <124866816+bagherzadeharia@users.noreply.github.com>
2024-03-22 22:52:28 +0330

    Create AddadeKhoudMaghloub.kt

---
+++ عدد خودمقلوب/AddadeKhoudMaghloub.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val mainString: String = readln()
+    val reversedString: String = mainString.reversed()
+    if(mainString==reversedString){
+        print("YES")
+    }else{
+        print("NO")
+    }
+}
