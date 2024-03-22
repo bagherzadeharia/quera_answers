91bbd91b9fe791506fee7bf3baa310728eb85d74
Aria Bagherzadeh <124866816+bagherzadeharia@users.noreply.github.com>
2024-03-22 18:32:12 +0330

    Create TekrareCodecup.kt

---
+++ تکرار کدکاپی/TekrareCodecup.kt
@@ -0,0 +1,6 @@
+fun main(){
+    val mainStr: String = "codecup6codecup6codecup6codecup6codecup6codecup6codecup6codecup6codecup6codecup6codecup6codecup6code"
+    val indexOfMainStr: Int = readln().toInt()
+    var charOfMainStr: Char = mainStr[indexOfMainStr-1]
+    print("$charOfMainStr")
+}
