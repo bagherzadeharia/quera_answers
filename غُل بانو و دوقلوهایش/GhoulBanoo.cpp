22f7a652b4eda93b16aa8a66145a0542abddc167
Aria Bagherzadeh <124866816+bagherzadeharia@users.noreply.github.com>
2024-04-01 15:32:51 +0330

    Update and rename GhoulBanoo.c to GhoulBanoo.cpp

---
+++ غُل بانو و دوقلوهایش/GhoulBanoo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string name;
+    cin >> name;
+    for(int i=(name.length()-1);i>=0;i--){
+        cout << name[I];
+    }
+    return 0;
+}
