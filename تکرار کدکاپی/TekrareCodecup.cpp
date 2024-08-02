287605d6461107dbff9e68e6d00a5e778efbb0b7
Aria Bagherzadeh <124866816+bagherzadeharia@users.noreply.github.com>
2024-08-02 17:35:03 +0330

    Update and rename TekrareCodecup.c to TekrareCodecup.cpp

---
+++ تکرار کدکاپی/TekrareCodecup.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int i;
+    string txt = "codecup6codecup6codecup6codecup6codecup6codecup6codecup6codecup6codecup6codecup6codecup6codecup6code";
+    cin >> i;
+    i -= 1;
+    cout << txt[i]
+    return 0;
+}
